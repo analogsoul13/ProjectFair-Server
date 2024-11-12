cc9c95c9847363ec10893c3328ba14209afcdb3a
analogsoul13 <analogsoul13@gmail.com>
2024-11-13 00:28:34 +0530

    Profile Update Fields

--- Models/userModel.js
+++ Models/userModel.js
@@ -14,6 +14,15 @@ const userSchema=new mongoose.Schema({
     password:{
         type:String,
         required:true
+    },
+    profile:{
+        type:String
+    },
+    linkedin:{
+        type:String
+    },
+    github:{
+        type:String
     }
 })
 
